78f9297b3d848887a3b36821ff183582d4cf2734
Israel Braga <ibraga1003@gmail.com>
2022-08-04 11:54:29 -0300

    H2 database

--- src/main/java/com/devsuperior/myfirstproject/entities/Category.java
+++ src/main/java/com/devsuperior/myfirstproject/entities/Category.java
@@ -1,16 +1,32 @@
 package com.devsuperior.myfirstproject.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
 public class Category implements Serializable {
 	
 	
 	private static final long serialVersionUID = 1L;
 	
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "category")
+	private List<Product> products = new ArrayList<>();
 	
 	public Category () {
 		
@@ -36,6 +52,11 @@ public class Category implements Serializable {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+	
+
+	public List<Product> getProducts() {
+		return products;
 	}
 
 	@Override
--- src/main/java/com/devsuperior/myfirstproject/repositories/ProductRepository.java
+++ src/main/java/com/devsuperior/myfirstproject/repositories/ProductRepository.java
@@ -3,9 +3,9 @@ package com.devsuperior.myfirstproject.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.devsuperior.myfirstproject.entities.Category;
+import com.devsuperior.myfirstproject.entities.Product;
 
 @Repository
-public interface CategoryRepository extends JpaRepository<Category, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
